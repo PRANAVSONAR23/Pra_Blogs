9105a2f37269db17c1f5b980134bd45e364d95e1
PRANAVSONAR23 <pranavsonar2311@gmail.com>
2024-12-13 01:23:12 +0530

    started auth

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express=require('express')
 const path =require('path')
+const mongoose=require('mongoose')
+
+const userRoute= require('./routes/user')
 
 const app=express()
 const port=8000
 
+mongoose.connect('mongodb+srv://pranavsonar2311:YDWetrxzL6C5vLqu@cluster0.wuaph.mongodb.net/blogify').then(e=>console.log('MongoDB Connected'))
+
 app.set('view engine','ejs');
 app.set('views', path.resolve("./views"));
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.get('/',(req,res)=>{
     res.render('home')
 })
+
+app.use('/user', userRoute)
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
