ef68b3ee5f8a5590945c9ed9ae9876391e34425e
PRANAVSONAR23 <pranavsonar2311@gmail.com>
2024-12-14 01:02:55 +0530

    blog uplaod

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
-const express=require('express')
-const path =require('path')
-const mongoose=require('mongoose')
-const cookieParser=require('cookie-parser')
-const userRoute= require('./routes/user')
-const {checkForAuthentication}=require('./middleware/authentication')
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+const { checkForAuthentication } = require("./middleware/authentication");
 
-const app=express()
-const port=8000
+const Blog = require("./models/blog");
 
-mongoose.connect('mongodb+srv://pranavsonar2311:YDWetrxzL6C5vLqu@cluster0.wuaph.mongodb.net/blogify').then(e=>console.log('MongoDB Connected'))
+const app = express();
+const port = 8000;
 
-app.set('view engine','ejs');
-app.set('views', path.resolve("./views"));
+mongoose
+  .connect(
+    "mongodb+srv://pranavsonar2311:YDWetrxzL6C5vLqu@cluster0.wuaph.mongodb.net/blogify"
+  )
+  .then((e) => console.log("MongoDB Connected"));
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(cookieParser())
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
-app.use(checkForAuthentication('token'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.resolve("./public")));
 
-app.get('/',(req,res)=>{
-    res.render('home',{
-        user:req.user
-    })
-})
+app.use(checkForAuthentication("token"));
 
-app.use('/user', userRoute)
+app.get("/", async (req, res) => {
+  const allblogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allblogs,
+  });
+});
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
